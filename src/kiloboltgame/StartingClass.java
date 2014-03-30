ae786da0dd035edf2b04a811acbb0423df04d2dd
Zachary3 <zachzla@gmail.com>
2014-03-30 13:00:46 +0100

    Day 3 finished

--- src/kiloboltgame/StartingClass.java
+++ src/kiloboltgame/StartingClass.java
@@ -59,21 +59,59 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 	}
 
 	@Override
-	public void keyPressed(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			System.out.println("Move up");
+			break;
+
+		case KeyEvent.VK_DOWN:
+			System.out.println("Move down");
+			break;
+
+		case KeyEvent.VK_LEFT:
+			System.out.println("Move left");
+			break;
+
+		case KeyEvent.VK_RIGHT:
+			System.out.println("Move right");
+			break;
+
+		case KeyEvent.VK_SPACE:
+			System.out.println("Jump");
+			break;
+		}
 	}
 
 	@Override
-	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public void keyReleased(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			System.out.println("Stop moving up");
+			break;
+
+		case KeyEvent.VK_DOWN:
+			System.out.println("Stop moving down");
+			break;
+
+		case KeyEvent.VK_LEFT:
+			System.out.println("Stop moving left");
+			break;
+
+		case KeyEvent.VK_RIGHT:
+			System.out.println("Stop moving right");
+			break;
+
+		case KeyEvent.VK_SPACE:
+			System.out.println("Stop jumping");
+			break;
+		}
 	}
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
