6c18d678829a7a1c08a9c5ad1419c53777d9fd30
Zachary3 <zachzla@gmail.com>
2014-03-30 12:37:50 +0100

    Day 2 finished

--- src/kiloboltgame/StartingClass.java
+++ src/kiloboltgame/StartingClass.java
@@ -1,13 +1,24 @@
 package kiloboltgame;
 
 import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Frame;
 
 public class StartingClass extends Applet implements Runnable {
 
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
-		super.init();
+
+		// sets applet size
+		setSize(800, 600);
+		// sets applet background
+		setBackground(Color.BLACK);
+		// applet takes focus and the input goes directly into it
+		setFocusable(true);
+		// assigns the applet window to the frame variable
+		Frame frame = (Frame) this.getParent().getParent();
+		// sets the title
+		frame.setTitle("Q-Bot Alpha");
 	}
 
 	@Override
@@ -30,8 +41,15 @@ public class StartingClass extends Applet implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		while (true) {
+			// this calls paint
+			repaint();
+			try {
+				Thread.sleep(17);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
