5faf12db327dec507df4838a47ef960a0149396a
Zachary3 <zachzla@gmail.com>
2014-04-06 13:03:17 +0100

    Adding getters and setters to Enemy class

--- src/kiloboltgame/Enemy.java
+++ src/kiloboltgame/Enemy.java
@@ -18,4 +18,60 @@ public class Enemy {
 	public void attack() {
 
 	}
+
+	public int getMaxHealth() {
+		return maxHealth;
+	}
+
+	public int getCurrentHealth() {
+		return currentHealth;
+	}
+
+	public int getPower() {
+		return power;
+	}
+
+	public int getSpeedX() {
+		return speedX;
+	}
+
+	public int getCenterX() {
+		return centerX;
+	}
+
+	public int getCenterY() {
+		return centerY;
+	}
+
+	public Background getBg() {
+		return bg;
+	}
+
+	public void setMaxHealth(int maxHealth) {
+		this.maxHealth = maxHealth;
+	}
+
+	public void setCurrentHealth(int currentHealth) {
+		this.currentHealth = currentHealth;
+	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	public void setSpeedX(int speedX) {
+		this.speedX = speedX;
+	}
+
+	public void setCenterX(int centerX) {
+		this.centerX = centerX;
+	}
+
+	public void setCenterY(int centerY) {
+		this.centerY = centerY;
+	}
+
+	public void setBg(Background bg) {
+		this.bg = bg;
+	}
 }
